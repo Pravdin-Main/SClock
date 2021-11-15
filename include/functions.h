7fff40f2e6ecbd0456d78771adc18d48b74fdef2
Pravdin-Main <70532298+Pravdin-Main@users.noreply.github.com>
2021-11-15 22:17:36 +0300

    test

--- include/functions.h
+++ include/functions.h
@@ -6,7 +6,7 @@
 //-------------------------- Modules ----------------------
 
 #define CLOCK 1       //1 - clock type 1; 2 - clock type 2; 3 - clock type 3; 4 - clock type 4
-#define ALARM 0       //0 - alarm module OFF; 1 - alarm module ON
+#define ALARM 1       //0 - alarm module OFF; 1 - alarm module ON
 #define OPTION 1      //0 - options OFF; 1 - options ON
 #define SENSORS 1     //0 - sensors OFF; 1 - sensors - ON
 #define SENS_CO2 1    //0 - CO2 sensor OFF; 1 - CO2 sensor ON
